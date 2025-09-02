4289a8c0def3a0d8e6aaf7872d08ecefe2ff8ab6
DominicDev <s0l0estudi4r@gmail.com>
2025-09-02 14:22:57 +0200

    v0.0.2

--- src/utils/functions.ts
+++ src/utils/functions.ts
@@ -2,6 +2,5 @@ export const $ = (el: string) => document.querySelector(el);
 export const $$ = (el: string) => document.querySelectorAll(el);
 
 export function baseUrl(path: string) {
-  return path;
   return new URL(path.replace(/^\/+/, ""), import.meta.env.SITE).toString();
 }
